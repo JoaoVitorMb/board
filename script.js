d46c0c7e44d781e8ce98a3bc0cbe2ea9e6d2ab3a
Joao <j.vitor_mb@hotmail.com>
2019-05-17 12:51:30 -0300

    commit main frontend main files

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function init () {
+	
+	console.log("Hello World!");
+	
+	/*
+	<tr>
+		<td>Project 1</td>
+		<td>Column 2</td>
+		<td>Column 3</td>
+		<td><a href="#">details</a></td>
+	</tr> */
+	
+}
+
+init();
